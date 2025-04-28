8fef0ea75f47f1751fdc4939929ea1f9644ad47f
Martin <martin.abilev@gmail.com>
2025-04-28 12:15:17 +0300

    feat:  add clone from audio

--- nari_clonevoice.py
+++ nari_clonevoice.py
@@ -25,19 +25,25 @@ except TypeError:
     print("Device parameter not supported, falling back to default.")
     model = Dia.from_pretrained("nari-labs/Dia-1.6B", compute_dtype="float16")
 
-# Input text
-text = "[S1] Dia is an open weights text to dialogue model. [S2] You get full control over scripts and voices. [S1] Wow. Amazing. (laughs) [S2] Try it now on Git hub or Hugging Face."
-# clone from audio
+# You should put the transcript of the voice you want to clone
+# We will use the audio created by running simple.py as an example.
+# Note that you will be REQUIRED TO RUN simple.py for the script to work as-is.
+clone_from_text = "[S1] Dia is an open weights text to dialogue model. [S2] You get full control over scripts and voices. [S1] Wow. Amazing. (laughs) [S2] Try it now on Git hub or Hugging Face."
 clone_from_audio = "simple.mp3"
 
-# Print start time
-print("START:", x.date(), x.now())
+# For your custom needs, replace above with below and add your audio file to this directory:
+# clone_from_text = "[S1] ... [S2] ... [S1] ... corresponding to your_audio_name.mp3"
+# clone_from_audio = "your_audio_name.mp3"
 
-# Generate output (ensure input is on GPU if required by the model)
-output = model.generate(text, use_torch_compile=True, verbose=True)
+# Text to generate
+text_to_generate = "[S1] Hello, how are you? [S2] I'm good, thank you. [S1] What's your name? [S2] My name is Dia. [S1] Nice to meet you. [S2] Nice to meet you too."
 
-# Save audio
-model.save_audio("simple.mp3", output)
+# It will only return the audio from the text_to_generate
+output = model.generate(
+    clone_from_text + text_to_generate, audio_prompt=clone_from_audio, use_torch_compile=True, verbose=True
+)
+
+model.save_audio("voice_clone.mp3", output)
 
 # Print end time
 print("READY:", x.date(), x.now())
